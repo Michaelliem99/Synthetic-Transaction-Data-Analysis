49f722dc387ffd58f725a7ce213c4f553429b7ba
Michaelliem99 <43264172+Michaelliem99@users.noreply.github.com>
2023-03-20 01:27:31 +0700

    Add Excel Download Button

--- pages/1_Transaction_Data_View.py
+++ pages/1_Transaction_Data_View.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from datetime import datetime, timedelta
+import xlsxwriter
+from io import BytesIO
 
 st.set_page_config('Transaction Data Overview', layout='wide', initial_sidebar_state='auto', menu_items=None)
 st.title('Transaction Data Overview')
@@ -136,3 +138,24 @@ filtered_df = df[
 #     st.dataframe(df)
 
 st.dataframe(filtered_df)
+
+# Download excel file
+## Filtered Data
+filtered_output = BytesIO()
+filtered_writer = pd.ExcelWriter(filtered_output, engine='xlsxwriter')
+
+filtered_df.to_excel(filtered_writer, index=False)
+filtered_writer.save()
+
+## All Data
+all_output = BytesIO()
+all_writer = pd.ExcelWriter(all_output, engine='xlsxwriter')
+df.to_excel(all_writer, index=False)
+all_writer.save()
+
+## Download Button
+dl_1, dl_2, _, _, _ = st.columns([1, 1, 1, 1, 1])
+with dl_1:
+    st.download_button("Download Filtered Data in Excel", data=filtered_output.getvalue(), file_name='transaction_filtered.xlsx', mime="application/vnd.ms-excel")
+with dl_2: 
+    st.download_button("Download All Data in Excel", data=all_output.getvalue(), file_name='transaction_all.xlsx', mime="application/vnd.ms-excel")
--- pages/2_Client_Data_View.py
+++ pages/2_Client_Data_View.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import xlsxwriter
+from io import BytesIO
 
 st.set_page_config('Client Data Overview', layout='wide', initial_sidebar_state='auto', menu_items=None)
 st.title('Client Data Overview')
@@ -55,3 +57,24 @@ filtered_df = df[
 
 # Print out dataframe
 st.dataframe(filtered_df)
+
+# Download excel file
+## Filtered Data
+filtered_output = BytesIO()
+filtered_writer = pd.ExcelWriter(filtered_output, engine='xlsxwriter')
+
+filtered_df.to_excel(filtered_writer, index=False)
+filtered_writer.save()
+
+## All Data
+all_output = BytesIO()
+all_writer = pd.ExcelWriter(all_output, engine='xlsxwriter')
+df.to_excel(all_writer, index=False)
+all_writer.save()
+
+## Download Button
+dl_1, dl_2, _, _, _ = st.columns([1, 1, 1, 1, 1])
+with dl_1:
+    st.download_button("Download Filtered Data in Excel", data=filtered_output.getvalue(), file_name='client_filtered.xlsx', mime="application/vnd.ms-excel")
+with dl_2: 
+    st.download_button("Download All Data in Excel", data=all_output.getvalue(), file_name='client_all.xlsx', mime="application/vnd.ms-excel")
